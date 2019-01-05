fdee0bdbab40b81e1720f81f7216655df70ddeb3
Al <gabriel.cortesi@outlook.com>
2019-01-05 15:05:05 +0100

    Add "NetScriptFramework" as valid top level directory.

--- src/installationtester.cpp
+++ src/installationtester.cpp
@@ -38,7 +38,7 @@ bool InstallationTester::isTopLevelDirectory(const FileNameString &dirName)
     "sound", "strings", "textures", "trees", "video", "facegen", "materials",
     "skse", "obse", "mwse", "nvse", "fose", "f4se", "distantlod", "asi",
     "SkyProc Patchers", "Tools", "MCM", "icons", "bookart", "distantland",
-    "mits", "splash", "dllplugins", "CalienteTools"
+    "mits", "splash", "dllplugins", "CalienteTools", "NetScriptFramework"
   };
 
   return tlDirectoryNames.count(dirName) != 0;
