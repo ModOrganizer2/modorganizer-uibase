2222b4688a288b6167f968ea286ee0f427e392f9
isanae <14251494+isanae@users.noreply.github.com>
2019-07-19 06:31:57 -0400

    added missing isavegame.h and isavegameinfowidget.h to cmakelist switched back to qCritical() so as not to pull fmt into plugins

--- src/iplugingame.h
+++ src/iplugingame.h
@@ -59,8 +59,10 @@ public:
       try {
         return boost::any_cast<T*>(iter->second);
       } catch (const boost::bad_any_cast&) {
-        log::error(
-          "failed to retrieve feature type {} (got {})",
+        // don't use log::error() here so log.h and fmt aren't pulled into
+        // plugins
+        qCritical(
+          "failed to retrieve feature type %s (got %s)",
           typeid(T).name(), typeid(iter->second).name());
         return nullptr;
       }
