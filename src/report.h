82aa7a7ce00a7c1ce81104ed6f15cd04c2aa0b7b
isanae <14251494+isanae@users.noreply.github.com>
2019-09-23 16:31:31 -0400

    missing qt includes

--- src/report.h
+++ src/report.h
@@ -25,6 +25,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <QString>
 #include <QMessageBox>
 #include <QPlainTextEdit>
+#include <QComboBox>
+#include <QCheckBox>
 #include <wchar.h>
 
 namespace Ui { class TaskDialog; }
