8d08e6f3425652e51b668385c7b35bc28422a891
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-20 15:43:00 +0200

    Add missing <stdexcept> header.

--- src/ifiletree.h
+++ src/ifiletree.h
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef IFILETREE_H
 #define IFILETREE_H
 
+#include <stdexcept>
 #include <map>
 #include <memory>
 #include <vector>
