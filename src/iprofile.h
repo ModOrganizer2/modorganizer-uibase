6ba6cd4ff34298774145fc39b94e97640f913c05
Mikaël Capelle <capelle.mikael@gmail.com>
2020-09-27 20:56:28 +0200

    Rename iniFilePath() -> absoluteIniFilePath().

--- src/iprofile.h
+++ src/iprofile.h
@@ -20,19 +20,20 @@ public:
   virtual bool invalidationActive(bool *supported) const = 0;
 
   /**
-   * @brief Retrieve the absolute file to the corresponding file.
+   * @brief Retrieve the absolute file path to the corresponding INI file for this
+   *     profile.
    *
    * @param iniFile INI file to retrieve a path for. This can either be the
    *     name of a file or a path to the absolute file outside of the profile.
    *
    * @return the absolute path for the given INI file for this profile.
    *
-   * @note If iniFile does not correspond to a file in the list of ini files for the 
+   * @note If iniFile does not correspond to a file in the list of INI files for the 
    *     current game (as returned by IPluginGame::iniFiles), the path to the global 
    *     file will be returned (if iniFile is absolute, iniFile is returned, otherwiise 
          the path is assumed relative to the game documents directory).
    */
-  virtual QString iniFilePath(QString iniFile) const = 0;
+  virtual QString absoluteIniFilePath(QString iniFile) const = 0;
 };
 
 } // namespace MOBase
