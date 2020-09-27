ba2f883533f9c446b2d87d235f56eaae795077f9
Mikaël Capelle <capelle.mikael@gmail.com>
2020-09-27 20:18:23 +0200

    Add IProfile::iniFilePath().

--- src/iprofile.h
+++ src/iprofile.h
@@ -18,6 +18,16 @@ public:
   virtual bool localSavesEnabled() const = 0;
   virtual bool localSettingsEnabled() const = 0;
   virtual bool invalidationActive(bool *supported) const = 0;
+
+  /**
+   * @brief Retrieve the absolute file to the corresponding file.
+   *
+   * @param iniFile INI file to retrieve a path for. This can either be the
+   *     name of a file or a path to the absolute file outside of the profile.
+   *
+   * @return the absolute path for the given INI file for this profile.
+   */
+  virtual QString iniFilePath(QString iniFile) = 0;
 };
 
 } // namespace MOBase
