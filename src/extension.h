4119dd6456cf1d242257364ac99a2fafc1a45456
Mikaël Capelle <capelle.mikael@gmail.com>
2022-05-23 19:29:14 +0200

    Implement loading theme and translation extensions.

--- src/extension.h
+++ src/extension.h
@@ -15,7 +15,6 @@
 
 namespace MOBase
 {
-
 class IExtension;
 
 class VersionRequirement
@@ -25,71 +24,77 @@ class GameRequirement
 class ExtensionRequirement
 {};
 
+enum class ExtensionType
+{
+  INVALID,
+  THEME,
+  TRANSLATION,
+  PLUGIN,
+  GAME
+};
+
 class QDLLEXPORT ExtensionMetaData
 {
 public:
-  ExtensionMetaData(const IExtension* extension, QJsonObject const& jsonData);
+  ExtensionMetaData(QJsonObject const& jsonData);
 
-  /**
-   * @return the name of the extension.
-   */
-  const auto& name() const { return localized(m_Name); }
+  // check if that metadata object is valid
+  //
+  bool isValid() const;
 
-  /**
-   * @return the description of the extension.
-   */
-  const auto& description() const { return localized(m_Description); }
+  // retrieve the identifier of the extension
+  //
+  const auto& identifier() const { return m_Identifier; }
 
-  /**
-   * @return the version of the extension.
-   */
+  // retrieve the name of the extension
+  //
+  auto name() const { return localized(m_Name); }
+
+  // retrieve the type of the extension
+  //
+  auto type() const { return m_Type; }
+
+  // retrieve the description of the extension.
+  //
+  auto description() const { return localized(m_Description); }
+
+  // retrieve the version of the extension.
+  //
   const auto& version() const { return m_Version; }
 
-  /**
-   * @return the version requirement of the extension.
-   */
+  // retrieve the version requirement of the extension
+  //
   const auto& versionRequirement() const { return m_VersionRequirement; }
 
-  /**
-   * @return the game requirement of the extension.
-   */
+  // retrieve the game requirement of the extension
+  //
   const auto& gameRequirement() const { return m_GameRequirement; }
 
-  /**
-   * @return the extension requirement of the extension.
-   */
+  // retrieve the extension requirement of the extension
+  //
   const auto& extensionRequirement() const { return m_ExtensionRequirement; }
 
+  // retrieve the raw JSON metadata, this is mostly useful for specific extension type
+  // to extract custom parts
+  //
+  const auto& json() const { return m_JsonData; }
+
 private:
-  QString localized(QString const& value) const
-  {
-    const auto result = QCoreApplication::translate(
-        m_TranslationContext.toUtf8().data(), value.toUtf8().data());
-    return result.isEmpty() ? value : result;
-  }
+  QString localized(QString const& value) const;
 
 private:
   constexpr static const char* DEFAULT_TRANSLATIONS_FOLDER = "translations";
   constexpr static const char* DEFAULT_STYLESHEET_PATH     = "stylesheets";
 
-private:
-  // these functions are only for IExtension
-  //
-  friend class IExtension;
-
-  // retrieve the prefix translations, if there is one, e.g., translations/foo
-  //
-  const auto& translationsFilePrefix() const { return m_TranslationFilesPrefix; }
-
-  // retrieve the path to the stylesheet, if there is one
-  //
-  const auto& styleSheetFilePath() const { return m_StyleSheetFilePath; }
+  ExtensionType parseType(QString const& value) const;
 
 private:
-  const IExtension* m_Extension;
+  QJsonObject m_JsonData;
   QString m_TranslationContext;
 
+  QString m_Identifier;
   QString m_Name;
+  ExtensionType m_Type;
   QString m_Description;
   VersionInfo m_Version;
 
@@ -104,61 +109,133 @@ private:
 class QDLLEXPORT IExtension
 {
 public:
-  /**
-   * @brief Retrieve the path to this extension folder.
-   *
-   * @return the directory of this extension.
-   */
+  // retrieve the folder containing the extension
+  //
   std::filesystem::path directory() const { return m_Path; }
 
-  /**
-   * @brief Retrieve the metadata of this extension.
-   *
-   * @return the metadata of this extension.
-   */
+  // retrieve the metadata of this extension
+  //
   const auto& metadata() const { return m_MetaData; }
-
-  /**
-   * @brief Retrieve the plugins from this extension. If the plugins have not been
-   * loaded yet, the plugins are loaded.
-   *
-   * @return the list of plugins from this extension.
-   */
-  std::vector<QObject*> plugins() const;
-
-  /**
-   * @brief Load the plugins from this extension.
-   *
-   * @return the list of plugins from this extension.
-   */
-  std::vector<QObject*> loadPlugins();
-
-  const auto& themes() const { return m_Themes; }
-  const auto& themeAdditions() const { return m_ThemeAdditions; }
-  const auto& translations() const { return m_Translations; }
-  const auto& translationAdditions() const { return m_TranslationAdditions; }
 
   virtual ~IExtension() {}
 
 protected:
   IExtension(std::filesystem::path path, ExtensionMetaData metadata);
 
-  // retrieve the plugins for this extension
-  //
-  virtual std::vector<QObject*> fetchPlugins() const = 0;
-
 private:
   std::filesystem::path m_Path;
   ExtensionMetaData m_MetaData;
+};
 
-  // theme and translations
+// factory for extensions
+//
+class QDLLEXPORT ExtensionFactory
+{
+public:
+  // load an extension from the given directory, return a null-pointer if the extension
+  // could not be load
+  //
+  static std::unique_ptr<IExtension> loadExtension(std::filesystem::path directory);
+
+private:
+  // name of the metadata file
+  //
+  static constexpr const char* METADATA_FILENAME = "mo2-metadata.json";
+
+  // load an extension from the given directory
+  //
+  static std::unique_ptr<IExtension> loadExtension(std::filesystem::path directory,
+                                                   ExtensionMetaData metadata);
+};
+
+// theme extension that provides one or more base themes for MO2
+//
+class QDLLEXPORT ThemeExtension : public IExtension
+{
+public:
+  // retrieve the list of themes provided by this extension
+  //
+  const auto& themes() const { return m_Themes; }
+
+private:
+  ThemeExtension(std::filesystem::path path, ExtensionMetaData metadata,
+                 std::vector<std::shared_ptr<const Theme>> themes);
+
+  friend class ExtensionFactory;
+  static std::unique_ptr<ThemeExtension> loadExtension(std::filesystem::path path,
+                                                       ExtensionMetaData metadata);
+
+  static std::shared_ptr<const Theme>
+  parseTheme(std::filesystem::path const& extensionFolder, const QString& identifier,
+             const QJsonObject& jsonTheme);
+
+private:
   std::vector<std::shared_ptr<const Theme>> m_Themes;
-  std::vector<std::shared_ptr<const ThemeAddition>> m_ThemeAdditions;
-  std::vector<std::shared_ptr<const Translation>> m_Translations;
-  std::vector<std::shared_ptr<const TranslationAddition>> m_TranslationAdditions;
+};
 
-  mutable bool m_Loaded{false};
-  mutable std::vector<QObject*> m_Plugins;
+// translation extension that provides one or more base translations for mo@
+//
+class QDLLEXPORT TranslationExtension : public IExtension
+{
+public:
+  // retrieve the list of translations provided by this extension
+  //
+  const auto& translations() const { return m_Translations; }
+
+private:
+  TranslationExtension(std::filesystem::path path, ExtensionMetaData metadata,
+                       std::vector<std::shared_ptr<const Translation>> translations);
+
+  friend class ExtensionFactory;
+  static std::unique_ptr<TranslationExtension>
+  loadExtension(std::filesystem::path path, ExtensionMetaData metadata);
+
+  static std::shared_ptr<const Translation>
+  parseTranslation(std::filesystem::path const& extensionFolder,
+                   const QString& identifier, const QJsonObject& jsonTranslation);
+
+private:
+  std::vector<std::shared_ptr<const Translation>> m_Translations;
+};
+
+// plugin extension that provides one or more plugins for MO2, alongside theme or
+// translation additions
+//
+class QDLLEXPORT PluginExtension : public IExtension
+{
+public:
+  using IExtension::IExtension;
+
+  // auto-detect plugins
+  //
+  bool autodetect() const { return m_AutoDetect; }
+
+  // list of specified plugins
+  //
+  const auto& plugins() const { return m_Plugins; }
+
+  const auto& themeAdditions() const { return m_ThemeAdditions; }
+  const auto& translationAdditions() const { return m_TranslationAdditions; }
+
+private:
+  // auto-detect plugins
+  bool m_AutoDetect;
+
+  // forced plugins
+  std::vector<std::filesystem::path> m_Plugins;
+
+  // theme and translations additions
+  std::vector<std::shared_ptr<const ThemeAddition>> m_ThemeAdditions;
+  std::vector<std::shared_ptr<const TranslationAddition>> m_TranslationAdditions;
+};
+
+// game extension that provides a game plugin, alongside other plugins, translation or
+// theme (additions)
+//
+class QDLLEXPORT GameExtension : public PluginExtension
+{
+public:
+  using PluginExtension::PluginExtension;
 };
 
 }  // namespace MOBase
