cea7a3f8ce85999196ef575b67fbf288a5c351bc
Twinki <thetwinkehh@gmail.com>
2024-05-17 14:34:01 -0400

    Add `displayGameName()` and `lootGameName()` to plugin-base
    # Modifications
    - Add `lootGameName()` which gives the plugin a tad bit more control over LOOT cli args
    - Add `displayGameName()` which will be used separate display-specific calls & more integral calls to `gameName()`, will also pave way for deeper plugin localization

--- src/iplugingame.h
+++ src/iplugingame.h
@@ -81,6 +81,16 @@ public:
    */
   virtual QString gameName() const = 0;
 
+  /**
+   * used to override display-specific text in place of the gameName
+   * for example the text for the main window or the initial instance creation game plugin selection/list
+   * 
+   * added for future translation purposes or to make plugins visually more accurate in the ui
+   * 
+   * @return display-specific name of the game
+   */
+  virtual QString displayGameName() const { return gameName(); }
+
   template <typename T>
   T* feature() const
   {
@@ -256,6 +266,14 @@ public:
    * nexus mod pages as far as I can see.
    */
   virtual QString gameShortName() const = 0;
+
+  /**
+   * @brief game name that's passed to the LOOT cli --game argument
+   * 
+   * only applicable for games using LOOT based sorting
+   * defaults to gameShortName()
+   */
+  virtual QString lootGameName() const { return gameShortName(); }
 
   /**
    * @brief Get any primary alternative 'short' name for the game
