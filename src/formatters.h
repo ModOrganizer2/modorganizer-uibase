50e21be1d92aef18a7d8f8eee9a4c51395187b05
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-13 22:31:05 +0200

    Add specialization of std::formatter for random-access containers.

--- src/formatters.h
+++ src/formatters.h
@@ -175,3 +175,187 @@ struct std::formatter<Enum, CharT> : std::formatter<std::underlying_type_t<Enum>
         static_cast<std::underlying_type_t<Enum>>(v), ctx);
   }
 };
+
+// random access container formatter (vector, QStringList, ...)
+
+namespace MOBase::details
+{
+template <typename Container>
+concept random_access_container = requires(Container const& c) {
+  {
+    c.begin()
+  } -> std::random_access_iterator;
+  {
+    c.end()
+  } -> std::random_access_iterator;
+};
+}  // namespace MOBase::details
+
+// formatter specialization for random access container, the default format is
+// [ a, b, c ] up to 3 elements, then [ a, b, ..., c ]
+//
+// it is possible to modify the output by changing the format string, check the test
+// file for examples
+//
+template <typename Container, class CharT>
+  requires MOBase::details::random_access_container<Container>
+struct std::formatter<Container, CharT>
+{
+  static constexpr CharT field_delimiter = '$';
+
+  // brackets to use
+  CharT open_bracket  = static_cast<CharT>('[');
+  bool space_bracket  = true;
+  CharT close_bracket = static_cast<CharT>(']');
+
+  // delimiter to use
+  basic_string_view<CharT> delimiter = ", ";
+
+  // maximum number of element to show
+  std::size_t max_show = 3;
+
+  template <class ParseContext>
+  constexpr CharT next_or_throw(ParseContext const& ctx,
+                                typename ParseContext::iterator it)
+  {
+    if (it == ctx.end()) {
+      throw std::format_error("invalid format args for random access container.");
+    }
+
+    return *it;
+  }
+
+  template <class ParseContext>
+  constexpr auto end_or_next(ParseContext const& ctx,
+                             typename ParseContext::iterator& it)
+  {
+    if (it == ctx.end() || *it == '}') {
+      return it;
+    }
+
+    if (*it == field_delimiter) {
+      return ++it;
+    }
+
+    return it;
+  }
+
+  template <class ParseContext>
+  constexpr auto parse_size(ParseContext& ctx, typename ParseContext::iterator it,
+                            std::size_t& value)
+  {
+    if (it == ctx.end() || *it == '}' || *it == field_delimiter) {
+      return it;
+    }
+    value = value * 10 + (*it++ - static_cast<CharT>('0'));
+    return parse_size(ctx, it, value);
+  }
+
+  template <class ParseContext>
+  constexpr auto find_end(ParseContext& ctx, typename ParseContext::iterator it)
+  {
+    if (it == ctx.end() || *it == '}' || *it == field_delimiter) {
+      return it;
+    }
+    return find_end(ctx, ++it);
+  }
+
+  template <class ParseContext>
+  constexpr ParseContext::iterator parseImpl(ParseContext& ctx,
+                                             typename ParseContext::iterator it)
+  {
+    if (it == ctx.end() || *it == '}') {
+      return it;
+    }
+
+    const auto c = *it++;
+    switch (c) {
+    case 'b':
+      open_bracket  = next_or_throw(ctx, it++);
+      close_bracket = next_or_throw(ctx, it++);
+
+      if (close_bracket == ' ') {
+        space_bracket = true;
+        close_bracket = next_or_throw(ctx, it++);
+      } else {
+        space_bracket = false;
+      }
+
+      it = end_or_next(ctx, it);
+      break;
+
+    case 'n': {
+      max_show = 0;
+      it       = parse_size(ctx, it, max_show);
+
+      if (*it == field_delimiter) {
+        it++;
+      }
+
+    } break;
+
+    case 'd': {
+      const auto start = it;
+      it               = find_end(ctx, it);
+      delimiter        = std::basic_string_view<CharT>(start, it);
+
+      if (*it == field_delimiter) {
+        it++;
+      }
+
+    } break;
+
+    default:
+      throw std::format_error("invalid format args for random access container.");
+    }
+
+    return parseImpl(ctx, it);
+  }
+
+  template <class ParseContext>
+  constexpr ParseContext::iterator parse(ParseContext& ctx)
+  {
+    return parseImpl(ctx, ctx.begin());
+  }
+
+  template <typename FmtIt, typename It>
+  FmtIt formatRange(FmtIt& out, It cur, It end) const
+  {
+    if (cur == end) {
+      return out;
+    }
+
+    out = std::format_to(out, "{}", *cur);
+    ++cur;
+
+    if (cur != end) {
+      out = std::format_to(out, "{}", delimiter);
+    }
+
+    return formatRange(out, cur, end);
+  }
+
+  template <typename FmtContext>
+  FmtContext::iterator format(Container v, FmtContext& ctx) const
+  {
+    const auto n = v.end() - v.begin();
+
+    if (n == 0) {
+      return std::format_to(ctx.out(), "{}{}", open_bracket, close_bracket);
+    }
+
+    auto out =
+        std::format_to(ctx.out(), "{}{}", open_bracket, space_bracket ? " " : "");
+
+    if (n <= max_show) {
+      out = formatRange(out, v.begin(), v.end());
+    } else {
+      const auto n_first = (max_show % 2 == 1) ? max_show / 2 + 1 : max_show / 2;
+      out                = formatRange(out, v.begin(), v.begin() + n_first);
+      out                = std::format_to(out, "{}...{}", delimiter, delimiter);
+      out                = formatRange(out, v.end() - (max_show - n_first), v.end());
+    }
+
+    return std::format_to(ctx.out(), "{}{}", space_bracket ? " " : "", close_bracket);
+  }
+};
