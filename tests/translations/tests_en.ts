10f6f71278f9d34665a769d87e581fbd97705fe2
Mikaël Capelle <capelle.mikael@gmail.com>
2024-08-09 12:47:13 +0200

    Allow for external mo2-cmake (#160)
    * Create standalone feature for mo2-cmake.
    * Setup a QCoreApplication in tests main.

---
+++ tests/translations/tests_en.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>uibase-tests</name>
+    <message>
+        <source>Translate to French</source>
+        <translation>Translate to French</translation>
+    </message>
+</context>
+</TS>
