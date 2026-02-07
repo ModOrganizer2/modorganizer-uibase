8f2f9124c40886f2ed74ce8067617517bc011ed7
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2026-02-07 11:36:28 +0100

    Remove low-level methods from IInstance and IInstanceManager - Remove const qualifiers from gameName and gameDirectory to allow for memoization

--- include/uibase/iinstance.h
+++ include/uibase/iinstance.h
@@ -34,69 +34,27 @@ class IInstance
 {
 public:
   /**
-   * @brief Read instance data from the INI file if not yet known.
-   *
-   * @note This includes the game name, game directory, base directory, and profile
-   * name.
-   *
-   * @return true on success, false on failure (e.g. INI file not found or invalid).
-   */
-  virtual bool readFromIni() = 0;
-
-  /**
    * @return The instance name; this is the directory name or "Portable" for portable
    * instances.
    */
   virtual QString displayName() const = 0;
 
   /**
-   * @return The name of the game managed by this instance, or an empty string if not
-   * yet read from the INI file.
-   *
-   * @note Only available after readFromIni() has been called successfully.
+   * @return The name of the game managed by this instance, or an empty string if the
+   * INI file could not be read.
    */
-  virtual QString gameName() const = 0;
+  virtual QString gameName() = 0;
 
   /**
-   * @return The directory where the game is installed, or an empty string if not yet
-   * read from the INI file.
-   *
-   * @note Only available after readFromIni() has been called successfully.
+   * @return The directory where the game is installed, or an empty string if the INI
+   * file could not be read.
    */
-  virtual QString gameDirectory() const = 0;
-
-  /**
-   * @return The instance directory.
-   */
-  virtual QString directory() const = 0;
-
-  /**
-   * @return The base directory configured for this instance, or an empty string if not
-   * yet read from the INI file.
-   *
-   * @note Only available after readFromIni() has been called successfully.
-   */
-  virtual QString baseDirectory() const = 0;
+  virtual QString gameDirectory() = 0;
 
   /**
    * @return true if this is a portable instance, false if it is a global one.
    */
   virtual bool isPortable() const = 0;
-
-  /**
-   * @return The name of the profile currently selected in this instance, or an empty
-   * string if not yet read from the INI file.
-   *
-   * @note Only available after readFromIni() has been called successfully.
-   */
-  virtual QString profileName() const = 0;
-
-  /**
-   * @return The path to the INI file used by this instance.
-   *
-   * @note This file may not exist.
-   */
-  virtual QString iniPath() const = 0;
 };
 }  // namespace MOBase
 
--- include/uibase/iinstancemanager.h
+++ include/uibase/iinstancemanager.h
@@ -37,101 +37,16 @@ class IInstanceManager
 {
 public:
   /**
-   * @brief Override the instance name found in the registry.
-   */
-  virtual void overrideInstance(const QString& instanceName) = 0;
-
-  /**
-   * @brief Override the profile found in the INI file of the current instance.
-   */
-  virtual void overrideProfile(const QString& profileName) = 0;
-
-  /**
-   * @brief Clear the instance and profile overrides (used when restarting Mod Organizer
-   * to select another instance).
-   */
-  virtual void clearOverrides() = 0;
-
-  /**
-   * @brief Clear the instance name from the registry.
-   *
-   * @note On restart, this will make Mod Organizer either select the portable instance
-   * if it exists, or display the instance selection/creation dialog.
-   */
-  virtual void clearCurrentInstance() = 0;
-
-  /**
    * @return the current instance.
    */
   virtual std::shared_ptr<IInstance> currentInstance() const = 0;
 
   /**
-   * @brief Set the instance name in the registry so it will be opened the next time Mod
-   * Organizer is started.
-   *
-   * @param instanceName Name of the instance to set as current.
-   */
-  virtual void setCurrentInstance(const QString& instanceName) = 0;
-
-  /**
-   * @return true if the user can change the current instance from the user interface.
-   */
-  virtual bool allowedToChangeInstance() const = 0;
-
-  /**
-   * @return true if a portable instance exists.
-   */
-  virtual bool portableInstanceExists() const = 0;
-
-  /**
-   * @return the absolute path to the portable instance, regardless of whether one
-   * exists.
-   */
-  virtual QString portablePath() const = 0;
-
-  /**
-   * @return the absolute path to the directory that contains global instances.
-   *
-   * @note This is typically "%LOCALAPPDATA%\ModOrganizer".
-   */
-  virtual QString globalInstancesRootPath() const = 0;
-
-  /**
    * @return The list of absolute paths to all global instances.
    *
-   * @note This does not include the portable instance.
+   * @note This does not include portable instances.
    */
   virtual std::vector<QString> globalInstancePaths() const = 0;
-
-  /**
-   * @brief Check if a global instance with the specified name exists.
-   *
-   * @param instanceName Name of the global instance to check.
-   *
-   * @return true if a global instance with the specified name exists, false otherwise.
-   */
-  virtual bool globalInstanceExists(const QString& instanceName) const = 0;
-
-  /**
-   * @brief Retrieve the absolute path to the specified global instance.
-   *
-   * @param instanceName Name of the global instance.
-   *
-   * @return the absolute path to the specified global instance.
-   */
-  virtual QString globalInstancePath(const QString& instanceName) const = 0;
-
-  /**
-   * @brief Retrieve the absolute path to the INI file for the specified instance
-   * directory.
-   *
-   * @param instanceDirectory The instance directory.
-   *
-   * @return the absolute path to the INI file of the specified instance.
-   *
-   * @note The file may not exist.
-   */
-  virtual QString iniPath(const QString& instanceDirectory) const = 0;
 };
 }  // namespace MOBase
 
